489bacb16a0b4abcb16325f22195736bc8c7511b
Ewura Ama Assiamah <108219987+Ewurama-A@users.noreply.github.com>
2023-06-03 00:31:34 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -78,6 +78,7 @@ stu_mean = np.around(stu_mean,0)
 external_script = ["https://tailwindcss.com/", {"src":"https://cdn.tailwindcss.com"}]
 
 app = Dash(__name__, external_scripts=external_script, meta_tags=[{'name':'viewport','content':'width=device-width,initial-scale=1.0,maximum-scale=1.2,minimum-scale=0.5,'}])
+server = app.server
 app.scripts.config.serve_locally = True
 
 
