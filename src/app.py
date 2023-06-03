50dccfa88bed6d826c65a26d662b15da15ca26bb
Ewura Ama Assiamah <108219987+Ewurama-A@users.noreply.github.com>
2023-06-03 00:26:58 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -9,8 +9,7 @@ import pandas as pd
 
 
 # save dataset in variable 'df'
-df = pd.read_csv("https://github.com/Ewurama-A/Unidatarender-app/blob/main/merged_Uniersity_data.csv?raw=true")
-
+df = pd.read_csv(r"https://github.com/Ewurama-A/Unidatarender-app/blob/main/merged_Uniersity_data.csv?raw=true")
 
 # Creating graph of international students per location using plotly
 location_graph = px.scatter(df.query("scores_teaching>50"),x= 'stats_number_students',y= 'stats_pc_intl_students',size = 'stats_number_students',color='location',hover_name='name',title = 'Percentage of International students',color_discrete_sequence=px.colors.qualitative.Set1)
@@ -78,8 +77,7 @@ stu_mean = np.around(stu_mean,0)
 
 external_script = ["https://tailwindcss.com/", {"src":"https://cdn.tailwindcss.com"}]
 
-app = Dash(__name__, external_scripts=external_script)
-server = app.server
+app = Dash(__name__, external_scripts=external_script, meta_tags=[{'name':'viewport','content':'width=device-width,initial-scale=1.0,maximum-scale=1.2,minimum-scale=0.5,'}])
 app.scripts.config.serve_locally = True
 
 
@@ -87,34 +85,35 @@ app.scripts.config.serve_locally = True
 app.layout = html.Div([
   html.H1("Unidata: Universities of the world", id ='home', className=" shadow-lg w-auto text-white text-4xl italic p-6 m-2 rounded-md text-center bg-[#3b0764] shadow-lg"),
  
+   html.Div([
+        html.A( href ='#home', id = 'home', children = 'Home', className = 'shadow-md transition-colors duration-500 ease-in-out hover:bg-[#cbd5e1] hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-sm text-white p-4 font-semibold cursor-pointer block italic bg-[#312e81] shadow-md'),
+        html.A( href ='#graph', id = 'graph', children = 'Graphs', className = 'shadow-md transition-colors duration-500  ease-in-out hover:bg-[#cbd5e1] underline hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-sm text-white p-4 font-semibold cursor-pointer block italic bg-[#312e81] shadow-md'),
+        html.A( href ='#info', id = 'info', children = 'University Information', className = 'shadow-md transition-colors  duration-500 ease-in-out hover:bg-[#cbd5e1] hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-sm text-white p-4 font-semibold cursor-pointer block italic bg-[#312e81] shadow-md')],
+        className = 'shadow-md flex flex-col w-auto p-2 m-2 mt-1 bg-[#312e81] divide-y-4 divide-white justify evenly opacity-75 space-x-4 mb-2 rounded-md md:flex-row md:divide-x-4 md:justify-start shadow-md'),
+    
   html.Div([  
     
-    html.Div([
-        html.A( href ='#home', id = 'home', children = 'Home', className = 'shadow-md transition-colors duration-500 ease-in-out hover:bg-[#4f46e5] hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-lg p-4 font-semibold cursor-pointer block italic  bg-[#cbd5e1] shadow-md'),
-        html.A( href ='#graph', id = 'graph', children = 'Graphs', className = 'shadow-md transition-colors duration-500  ease-in-out hover:bg-[#4f46e5] underline hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-lg p-4 font-semibold cursor-pointer block italic bg-[#cbd5e1] shadow-md'),
-        html.A( href ='#info', id = 'info', children = 'University Information', className = 'shadow-md transition-colors  duration-500 ease-in-out hover:bg-[#4f46e5] hover:opacity-75 transform hover:-translate-y-1 hover:scale-100 rounded-lg p-4 font-semibold cursor-pointer block italic  bg-[#cbd5e1] shadow-md')],
-        className = 'shadow-md p-2 m-2 bg-[#312e81]  opacity-75 space-y-12  w-auto mb-2 rounded-md shadow-md'),
-    
+     
     html.Div([
       html.Div([
         
         html.Div(children=[
-             html.Span(uni_num, className="text-5xl text-center my-2 font-bold"),
-             html.Span("Universities available", className="text-lg fontmedium m-4 text-center"),
-         ],className=" shadow-lg flex flex-col justify-center p-4  text-white itemscenter rounded-md bg-[#4c1d95] shadow-lg"),
+             html.Span(uni_num, className="text-5xl text-center my-2 font-bold md:my-2 "),
+             html.Span("Universities available", className="text-lg fontmedium text-center md:m-4"),
+         ],className=" shadow-lg flex flex-col justify-center p-2 text-white itemscenter rounded-md bg-[#4c1d95] md:p-0 shadow-lg"),
          
            html.Div(children=[
              html.Span(location_num, className="text-5xl text-center my-2 font-bold"),
-             html.Span("Total locations available", className="text-lg fontmedium ml-4 text-center"),
-         ],className=" shadow-lg flex flex-col justify-center p-4  text-white itemscenter rounded-md  bg-[#6d28d9] shadow-lg"),
+             html.Span("Total locations available", className="text-lg fontmedium m-4 text-center"),
+         ],className=" shadow-lg flex flex-col justify-center p-4 text-white itemscenter rounded-md  bg-[#6d28d9] shadow-lg md:p-4"),
         
             html.Div(children=[
               html.Span(stu_mean, className="text-5xl text-center my-2 font-bold"),
               html.Span("Average students in a University", className="text-lg fontmedium ml-1 text-center"),
-         ],className=" shadow-lg flex flex-col justify-center p-4 text-white itemscenter rounded-md bg-[#4c1d95] shadow-lg")],className="flex flex-row justify-center rounded-md w-auto p-4 m-4 space-x-6 bg-[#e4e4e7] ",id = 'homebar'),
-    
+         ],className=" shadow-lg flex flex-col justify-center p-4 text-white itemscenter rounded-md bg-[#4c1d95] shadow-lg md:p-4")],className="flex flex-col justify-center rounded-md w-auto p-4 m-4 space-y-8 bg-[#e4e4e7] md:flex-row md:justify-center md:space-x-12",id = 'home'),
+     
        html.Div([
-         html.Div(dcc.Graph(id='location_graph',figure =location_graph),className="shadow-lg  rounded-md row-span-2 shadow-lg"),
+         html.Div(dcc.Graph(id='locationgraph',figure =location_graph),className="shadow-lg  rounded-md row-span-2 shadow-lg"),
          html.Div(dcc.Graph(figure=world_map),className="shadow-lg row-span-2 rounded-md  shadow-lg"),
          html.Div(dcc.Graph(id = 'gender_graph', figure = gender_graph),className="shadow-lg  rounded-md row-span-2 shadow-lg"),
          html.Div([
@@ -123,14 +122,14 @@ app.layout = html.Div([
              html.P(children = "Many Universities are available. Choose one to learn more about it.", id = 'output',className = "shadow-lg p-2 col-span-2 m-2 p-2 italic font-semibold justify-center text-base text-left whitespace-pre-line bg-[#e4e4e7] shadow-lg"),
          ],className="shadow-lg grid grid-cols-2 grid-row-3 gap-2 p-2 space-y-2 justify-center text-black itemscenter rounded-md bg-[#6d28d9] row-span-2 shadow-lg ")
            
-           ],id= 'graphbar', className='grid grid-cols-2 grid-rows-4 rounded-md  gap-4 m-2 px-4 py-8 bg-[#e4e4e7]')
+           ],id= 'graph', className='grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-4 rounded-md gap-8 md:gap-4 m-2 px-4 py-8 bg-[#e4e4e7]')
         
-    ],className = 'col-span-3'),
+    ],className = 'md:row-span-3 w-auto'),
         
-   ],className = 'flex flex-row grid grid-cols-4 gap-2'),
+   ],className = 'grid md:grid-rows-3 gap-2 w-full'),
     
-   html.Div(children = "Ama Assiamah. Data Copyright: Kaggle. @2023", className =" w-full text-white text-md italic p-4 text-center bg-[#4c1d95]")],
-   className="flex flex-col w-full bg-[#e4e4e7] min-w-full lg:max-w-full")
+   html.Div(children = "Ama Assiamah. Data Copyright: Kaggle. @2023", id= 'info', className =" w-auto text-white text-md italic p-4 text-center bg-[#4c1d95]")],
+   className="flex flex-col bg-[#e4e4e7] max-w-full xs:min-w-0 ")
 
 
 @app.callback(
